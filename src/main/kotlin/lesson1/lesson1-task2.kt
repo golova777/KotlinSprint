a9cb04ee49ae808f89bfadec781339003eae3373
Тарас <frost.44@ya.ru>
2024-09-17 17:38:21 +0300

    KS-1-1 create variables

---
+++ src/main/kotlin/lesson1/lesson1-task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Молодец, что у нас прикупил"
+
+    println(thankYouMessage)
+    println(thankYouMessage)
+
+    var numberOfEmployees: Int = 2000
+    //println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
