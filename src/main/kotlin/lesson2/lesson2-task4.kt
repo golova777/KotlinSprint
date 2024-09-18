2de1356b6ff54b9270237017e3d144a440544b03
Тарас <frost.44@ya.ru>
2024-09-18 22:43:54 +0300

    KS-2-4 buff effect percent refactor

--- src/main/kotlin/lesson2/lesson2-task4.kt
+++ src/main/kotlin/lesson2/lesson2-task4.kt
@@ -1,13 +1,16 @@
 package org.example.lesson2
 
+const val PERCENTAGE_DIVIDER = 100
+
+
 fun main() {
 
-    val buffEffectValue = 0.2f
+    val buffEffectValue = 20.0f
     val crystalOreValue = 7
     val ironOreValue = 11
 
-    val buffedCrystalOreValue = crystalOreValue * buffEffectValue
-    val buffedIronOreValue = ironOreValue * buffEffectValue
+    val buffedCrystalOreValue = crystalOreValue * (buffEffectValue / PERCENTAGE_DIVIDER)
+    val buffedIronOreValue = ironOreValue * (buffEffectValue / PERCENTAGE_DIVIDER)
 
 
     println("Бафф кристаллической руды: ${buffedCrystalOreValue.toInt()}")
