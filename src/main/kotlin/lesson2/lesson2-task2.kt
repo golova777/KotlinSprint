4b4b4e162135ad23134ae5bdcb544d5301571c6a
Тарас <frost.44@ya.ru>
2024-09-18 14:14:42 +0300

    KS-2-2 calc salary

---
+++ src/main/kotlin/lesson2/lesson2-task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson2
+
+
+fun main() {
+
+    val numberStaffEmployees = 50
+    val staffEmployeeSalary = 30000
+    val numberOfInterns = 30
+    val internSalary = 20000
+
+    val staffEmployeeTotalSalary = numberStaffEmployees * staffEmployeeSalary
+    val totalSalary = staffEmployeeTotalSalary + (numberOfInterns * internSalary)
+    val averageSalary = totalSalary / (numberStaffEmployees + numberOfInterns)
+
+    println(staffEmployeeTotalSalary)
+    println(totalSalary)
+    println(averageSalary)
+}
