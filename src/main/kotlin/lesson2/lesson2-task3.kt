08a5b28ab4e0d41d70fe6ee977839f1b8d2d72f7
Тарас <frost.44@ya.ru>
2024-09-19 20:26:07 +0300

    KS-2-3 make schedule #refactor-3

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -6,13 +6,12 @@ const val SECONDS_IN_MINUTE = 60
 fun main() {
 
     val trainDepartureTimeHours = 9
-    val trainDepartureTimeminutes = 39
+    val trainDepartureTimeMinutes = 39
     val trainRouteTimeMinutes = 475
 
-    val minutesSum = trainDepartureTimeminutes + trainRouteTimeMinutes
+    val minutesSum = trainDepartureTimeMinutes + trainRouteTimeMinutes
     val trainArraivingHour = trainDepartureTimeHours + ( minutesSum / SECONDS_IN_MINUTE )
-    val trainArraivingMinute = minutesSum % SECONDS_IN_MINUTE
+    val trainArrivingMinute = minutesSum % SECONDS_IN_MINUTE
 
-
-    println(String.format("%02d:%02d", trainArraivingHour, trainArraivingMinute))
+    println(String.format("%02d:%02d", trainArraivingHour, trainArrivingMinute))
 }
