91462176926e2349894bac2e789e0169a3546145
Тарас <frost.44@ya.ru>
2024-09-19 08:51:12 +0300

    KS-3-2 print FIO

---
+++ src/main/kotlin/lesson3/lesson3-task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    var userName = "Татьяна"
+    var userMiddleName = "Сергеевна"
+    var userLastName = "Андреева"
+
+    var userAge = 20
+
+    println("$userLastName $userName $userMiddleName, $userAge")
+
+    userLastName = "Сидорова"
+    userAge = 22
+    println("$userLastName $userName $userMiddleName, $userAge")
+}
