ecfef83e46ef9842b6b3c455c778df05d8fadd57
Тарас <frost.44@ya.ru>
2024-09-19 08:43:05 +0300

    KS-3-1 day night

---
+++ src/main/kotlin/lesson3/lesson3-task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main() {
+
+    val userName = "Фродо"
+
+    var greeting = "Славного вечера, " + userName + "!"
+    println(greeting)
+
+    greeting = "Дорое утро, " + userName + "!"
+    println(greeting)
+}
