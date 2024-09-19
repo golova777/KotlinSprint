dc81fb23b753a3e3b04f7deeb8ba9336755655b0
Тарас <frost.44@ya.ru>
2024-09-19 10:28:59 +0300

    KS-3-4 chess

---
+++ src/main/kotlin/lesson3/lesson3-task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    var fromPosition = "E2"
+    var toPosition = "E4"
+    var turnNumber = 1
+
+    println("$fromPosition-$toPosition:$turnNumber")
+
+    fromPosition = "D2"
+    toPosition = "D3"
+    turnNumber = 2
+
+    println("$fromPosition-$toPosition:$turnNumber")
+}
