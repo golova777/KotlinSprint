afcfc843891f671bb1c9d6e67b59c0fa6b5b6384
Тарас <frost.44@ya.ru>
2024-09-19 11:13:27 +0300

    KS-1-5 Calc seconds # consts differ

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -2,13 +2,15 @@ package org.example.lesson1
 
 const val SECONDS_IN_HOUR: Int = 3600
 const val SECONDS_IN_MINUTE: Int = 60
-const val SECONDS_IN_SPACE_GAGARIN: Int = 6480
+
 
 
 fun main() {
-    val hoursInSpace: Int = SECONDS_IN_SPACE_GAGARIN / SECONDS_IN_HOUR
-    val minutesRemainder: Int = (SECONDS_IN_SPACE_GAGARIN % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
-    val secondsRemainder: Int = SECONDS_IN_SPACE_GAGARIN % SECONDS_IN_MINUTE
+    val secondsInSpaceGagarin: Int = 6480
+
+    val hoursInSpace: Int = secondsInSpaceGagarin / SECONDS_IN_HOUR
+    val minutesRemainder: Int = (secondsInSpaceGagarin % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val secondsRemainder: Int = secondsInSpaceGagarin % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hoursInSpace, minutesRemainder, secondsRemainder))
 
