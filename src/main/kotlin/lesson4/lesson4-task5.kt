34e709d1a6506c26ca2e74112ed90b39362a6ea8
Тарас <frost.44@ya.ru>
2024-09-24 22:26:02 +0300

    KS-4-5 scientific ship #refactor 1

--- src/main/kotlin/lesson4/lesson4-task5.kt
+++ src/main/kotlin/lesson4/lesson4-task5.kt
@@ -1,5 +1,10 @@
 package org.example.lesson4
 
+const val MIN_CREW = 55
+const val RECOMENDED_CREW = 70
+const val OPTIMAL_PROVISION_BOXES = 50
+
+
 fun main() {
     print("Есть наличие повреждений корпуса? (да/нет): ")
     var isHullBreached: Boolean = readln() == "да"
@@ -10,9 +15,12 @@ fun main() {
     print("Благоприятны ли метеоусловия? (да/нет): ")
     var isMeteoGood: Boolean = readln() == "да"
 
-    var canShipTravel: Boolean = (!isHullBreached && (crewNumber >= 55 && crewNumber <= 70) &&
-                                 provisionBoxes > 50) ||
-                                 (crewNumber == 70 && isMeteoGood && provisionBoxes >= 50)
+    var canShipTravel: Boolean = (!isHullBreached &&
+                                 (crewNumber >= MIN_CREW && crewNumber <= RECOMENDED_CREW) &&
+                                 provisionBoxes > OPTIMAL_PROVISION_BOXES) ||
+                                 (crewNumber == RECOMENDED_CREW &&
+                                         isMeteoGood &&
+                                         provisionBoxes >= OPTIMAL_PROVISION_BOXES)
 
     println("Может ли корабль отправиться в плавание? $canShipTravel")
 }
